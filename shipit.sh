e4f917a5f57d7bf2d204981ef53f685bdbf95b2e
jamesacampbell <james@jamescampbell.us>
2020-10-30 15:34:42 -0400

    Trying to fake a conflict

--- shipit.sh
+++ shipit.sh
@@ -2,7 +2,10 @@ rm index.html
 mdslide -i deck.md -o index.html
 rm -rf css
 rm -rf js 
-mkdir -p js css
-cp theme/js/* js/
-cp theme/css/* css/
+rm -rf public/
+mkdir -p public/{assets,css,js}
+cp theme/js/* public/js/
+cp theme/css/* public/css/
+cp assets/* public/assets/
+cp index.html public/
 git yolo
